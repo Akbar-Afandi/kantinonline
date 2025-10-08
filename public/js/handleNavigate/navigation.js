02544b569efb0435b447c05c0d31adfdc8b945be
Akbar Afandi <greatemperorvoid@gmail.com>
2025-10-08 07:42:11 +0700

    Add files via upload

---
+++ public/js/handleNavigate/navigation.js
@@ -0,0 +1,13 @@
+const homeNav = document.getElementById("homeNav")
+const menuNav = document.getElementById("menuNav")
+const cartNav = document.getElementById("cartNav")
+
+homeNav.addEventListener("click", () => {
+  window.location.href = "beranda.html"
+})
+menuNav.addEventListener("click", () => {
+  window.location.href = "menu.html"
+})
+cartNav.addEventListener("click", () => {
+  window.location.href = "beranda.html"
+})
